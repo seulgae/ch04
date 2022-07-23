bc8b8a0e547d0c63d9d69256444856466a78b8a9
seulgae <r1605866@naver.com>
2022-07-23 20:42:32 +0900

    7강 게시판 검색 기능 추가하기(1) 41:45 검증완료.

--- src/main/java/com/fastcampus/ch4/dao/BoardDaoImpl.java
+++ src/main/java/com/fastcampus/ch4/dao/BoardDaoImpl.java
@@ -62,5 +62,14 @@ public class BoardDaoImpl implements BoardDao {
         return session.update(namespace+"increaseViewCnt", bno);
     } // int update(String statement, Object parameter)
 
+    @Override
+    public List<BoardDto> searchSelectPage(SearchCondition sc) throws Exception {
+        return session.selectList(namespace+"searchSelectPage", sc);
+    } // List<E> selectList(String statement, Object parameter)
+
+    @Override
+    public int searchResultCnt(SearchCondition sc) throws Exception {
+        return session.selectOne(namespace+"searchResultCnt", sc);
+    } // T selectOne(String statement, Object parameter)
 
 }
--- src/test/java/com/fastcampus/ch4/dao/BoardDaoImplTest.java
+++ src/test/java/com/fastcampus/ch4/dao/BoardDaoImplTest.java
@@ -18,6 +18,34 @@ public class BoardDaoImplTest {
     private BoardDao boardDao;
 
     @Test
+    public void searchSelectPage() throws Exception {
+        boardDao.deleteAll();
+        for (int i = 0; i <= 20; i++) {
+            BoardDto boardDto = new BoardDto("title"+i, "asdfadfassdf", "asdf");
+            boardDao.insert(boardDto);
+        }
+
+        SearchCondition sc = new SearchCondition(1, 10, "title2", "T"); // title1%
+        List<BoardDto> list = boardDao.searchSelectPage(sc);
+        System.out.println("list = " + list);
+        assertTrue(list.size()==2); // 1~20, title2, title20
+    }
+
+    @Test
+    public void searchResultCntTest() throws Exception {
+        boardDao.deleteAll();
+        for (int i = 0; i <= 20; i++) {
+            BoardDto boardDto = new BoardDto("title"+i, "asdfadfassdf", "asdf");
+            boardDao.insert(boardDto);
+        }
+
+        SearchCondition sc = new SearchCondition(1, 10, "title2", "T"); // title1%
+        int cnt = boardDao.searchResultCnt(sc);
+        System.out.println("cnt = " + cnt);
+        assertTrue(cnt==2); // 1~20, title2, title20
+    }
+
+    @Test
     public void insertTestData() throws Exception{
         boardDao.deleteAll();
         for (int i=1;i<=220;i++){
